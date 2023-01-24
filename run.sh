d4a41406665cec1e781365231a22b3f121880771
Josh Chambers <jc4gavejc@gmail.com>
2023-01-24 03:10:54 +0000

    add terraform stuff to .gitignore

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ go build -o terraform-provider-synovmm
 mv terraform-provider-synovmm ~/.terraform.d/plugins/terraform.local/m-bers/synovmm/0.2/linux_arm64/
 cd terraform
 rm -rf .terraform terraform.tfstate .terraform.lock.hcl
-# terraform init
-# terraform apply -auto-approve
+terraform init
+terraform apply -auto-approve
 cd ..
